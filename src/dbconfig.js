030076a9dd667b1847b8547d163d213976928565
cesaredm <cesaredmaradiaga@outlook.com>
2020-11-18 13:40:06 -0600

    c

--- src/dbconfig.js
+++ src/dbconfig.js
@@ -1,15 +1,15 @@
-/*const config = {
+const config = {
     host:'bjoraqzacwltasd8tins-mysql.services.clever-cloud.com',
     user: 'udigwa8nmzk2vxqu',
     password: 'xfm0bNCHhWd29s9dozOx',
     database:'bjoraqzacwltasd8tins'
-};*/
+};
     
-const config = {
+/*const config = {
     host:'localhost',
     user: 'root',
     password: '19199697tsoCD',
     database:'clinica'
-};   
+};  */ 
 
 module.exports = config;
